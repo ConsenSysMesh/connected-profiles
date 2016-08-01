43f77f302c482eb6b49ae4b96a1534cc3fa79301
Niran Babalola <niran@niran.org>
2016-08-01 02:07:49 -0500

    Concatenate multiline strings so build tools don't mangle them

--- src/services/reddit/templates.js
+++ src/services/reddit/templates.js
@@ -4,18 +4,16 @@ import _ from 'lodash';
 
 export const proofTitle = _.template('I control Ethereum account ${address}.');
 
-const PROOF_TEXT_TEMPLATE = "\
-I'm posting this to prove that **/u/${username}** controls the Ethereum account \
-**${address}**. This proof lets me use my username in [${appName}](${appUrl}) \
-and in any other Ethereum-based app.\
-";
+const PROOF_TEXT_TEMPLATE =
+"I'm posting this to prove that **/u/${username}** controls the Ethereum account " +
+'**${address}**. This proof lets me use my username in [${appName}](${appUrl}) ' +
+'and in any other Ethereum-based app.';
 
 export const proofText = _.template(PROOF_TEXT_TEMPLATE);
 
-export const proofUpdateText = _.template(PROOF_TEXT_TEMPLATE + '\n\
-\n\
-I registered this proof with a [transaction](${txUrl}) that contains a \
-[reference to my claim](${ipfsUrl}). That claim is reproduced below.\n\
-\n\
-${claimRecord}\n\
-');
+export const proofUpdateText = _.template(PROOF_TEXT_TEMPLATE + '\n' +
+'\n' +
+'I registered this proof with a [transaction](${txUrl}) that contains a ' +
+'[reference to my claim](${ipfsUrl}). That claim is reproduced below.\n' +
+'\n' +
+'${claimRecord}\n');
